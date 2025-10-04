dd35e010472562daf87805ad269a23eb9f8726d4
vad <oohavadlamudi.31@gmail.com>
2025-10-04 15:16:12 -0500

    question formate

--- quizapp/src/main/java/com/telusko/quizapp/Model/Question.java
+++ quizapp/src/main/java/com/telusko/quizapp/Model/Question.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -13,6 +16,10 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
+
+    @NotBlank(message = "Question cannot be blank")
+    @Size(min = 50, message = "Question must contain more than 50 letters")
+    @Pattern(regexp = "^[A-Za-z\\s?.,'\"!()]+$", message = "Question must be in alphabetical order")
     private String questionTitle;
     private String option1;
     private String option2;
